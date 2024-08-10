0284230f82b50854b0e4cab139260210ded93af9
manpreet1462 <80613332+manpreet1462@users.noreply.github.com>
2024-08-11 04:00:04 +0530

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,7 +5,7 @@ export const ProjectCard=({title,description,imgUrl})=>{
     return (
         <Col sm={6} md={4}>
             <div className="proj-imgbx">
-                <img src={imgUrl}  />
+                <img src={imgUrl} alt="" />
                 <div className="proj-txtx">
                     <h4>{title}</h4>
                     <span>{description}</span>
